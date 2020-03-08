e20339df952732823b3def36535a33c3695e7456
Mary Kang <mary.kang09@gmail.com>
2020-03-08 22:36:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -74,6 +74,7 @@ def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
   final_cart = apply_clearance(couponed_cart)
+  
   total = 0 
   
 end
