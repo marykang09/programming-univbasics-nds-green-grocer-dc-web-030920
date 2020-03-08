7ff61fdba0fb56ddb51d9b9b32dc96dfa911d7e4
Mary Kang <mary.kang09@gmail.com>
2020-03-08 22:06:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,6 +37,7 @@ def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    
     conter += 1
   end
 end
