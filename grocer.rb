81d335ca689306dae8e4c6b8baca98199322bdf1
Mary Kang <mary.kang09@gmail.com>
2020-03-08 21:39:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ counter = 0
 while counter < cart.length
   new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
   if new_cart_item != nil
-    
+    new_cart_item[:count] += 1
   end
   counter += 1
   end
