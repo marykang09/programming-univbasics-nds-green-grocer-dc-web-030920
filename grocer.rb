a65d76c89736c684de829f752e370a1e42a884d5
Mary Kang <mary.kang09@gmail.com>
2020-03-08 21:43:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -35,9 +35,7 @@ while counter < cart.length
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+
 end
 
 def apply_clearance(cart)
