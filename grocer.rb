014bec640e23ae310cfb07b42ffa28b6a4f5ee8d
Mary Kang <mary.kang09@gmail.com>
2020-03-08 22:19:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[counter][:cost] / coupons[counter][:num],
-          :clearance =>
+          :clearance => cart_item[:clearance]
           :count => coupons[counter][:num]
         }
       end
